cac007b0e91874595db733192ce900460ea41fec
a-gaoba <abdulrahman1997abdo@gmail.com>
2024-12-22 13:43:51 +0300

    first comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/home/Homepage";
+// import Home from "./pages/home/Homepage";
 import Almouhaqia from "./pages/almouhaqia/Home";
 
 
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/almouhaqia" element={<Almouhaqia />} />
         </Routes>
         <Footer />
