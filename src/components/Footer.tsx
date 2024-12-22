e7fca259e93a1f83659c34be54ad704c6d5030b9
a-gaoba <abdulrahman1997abdo@gmail.com>
2024-12-22 13:54:18 +0300

    first comment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="py-4 ">
-      <div className="px-5 sm:px-10 md:px-12 lg:px-5">
-        <div className="p-6 sm:p-10 md:p-14 lg:p-8 rounded-lg bg-gray-100 flex flex-col space-y-6 relative">
+    <footer className="py-4 overflow-hidden">
+      <div className="px-5 sm:px-10 md:px-12 lg:px-5 max-w-screen overflow-hidden">
+        <div className="p-6 sm:p-10 md:p-14 lg:p-8 rounded-lg bg-gray-100 flex flex-col space-y-6 relative overflow-hidden">
           {/* Background Effects */}
           <div className="absolute w-14 h-14 rounded-full bg-gradient-to-bl from-red-500 to-black blur-2xl z-10 -top-7 -left-7 opacity-40"></div>
           <div className="absolute w-14 h-14 rounded-full bg-gradient-to-bl from-red-500 to-black blur-2xl z-10 -bottom-7 -right-7 opacity-40"></div>
