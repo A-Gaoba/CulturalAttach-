8c16f292188b0ec787f25367db1472a96cbb278a
a-gaoba <abdulrahman1997abdo@gmail.com>
2024-12-28 18:40:35 +0300

    first comment

--- src/pages/CulturalActivities/Photos.tsx
+++ src/pages/CulturalActivities/Photos.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
 const ImageGallery = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
   const images = [
     "https://plus.unsplash.com/premium_photo-1674156433236-2338418ec4d9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://plus.unsplash.com/premium_photo-1674156886972-14c9a172bc50?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -7,6 +12,16 @@ const ImageGallery = () => {
     "https://plus.unsplash.com/premium_photo-1697729504520-e747e4ce1b6e?q=80&w=1986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1642425149819-af1b803b2b25?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   ];
+
+  const openModal = (src: string) => {
+    setSelectedImage(src);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
 
   return (
     <div className="mb-4">
@@ -21,14 +36,34 @@ const ImageGallery = () => {
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
+                  className="block h-full w-full rounded-lg object-cover object-center cursor-pointer"
                   src={src}
+                  onClick={() => openModal(src)}
                 />
               </div>
             </div>
           ))}
         </div>
       </div>
+
+      {/* Modal */}
+      {isModalOpen && selectedImage && (
+        <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
+          <div className="relative">
+            <button
+              className="absolute top-2 right-2 text-white text-2xl"
+              onClick={closeModal}
+            >
+              X
+            </button>
+            <img
+              src={selectedImage}
+              alt="Selected"
+              className="max-w-full max-h-full rounded-lg"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
